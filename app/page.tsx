5b2502d2beea655001ee6c2d04e83cd6b22562a7
Joshua Lehman <joshlehman.dev@gmail.com>
2024-01-31 00:13:29 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <>
+      <div>
+        <div className="header">
+          <h1 className="site-header">Luxe & Co.</h1>
+        </div>
+        <div className="content">
+          <a href="/" className="back-button">
+            ← Back
           </a>
+          <div className="page-layout">
+            <p id="title-mobile" className="p-title">
+              Product Name Goes Here
+            </p>
+            <div className="assets-container">
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-side-180"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-stand-front"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-side-90"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-bottom"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-top-45"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div className="asset-img">
+                <div
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-top-320"
+                  data-d8s-url-params="f_auto,w_1000"
+                ></div>
+              </div>
+              <div id="three-d-container">
+                <div
+                  id="three-d-preview"
+                  className="product-image preview-image"
+                  data-d8s-preset="shoes"
+                  data-d8s-name="sample-side-90"
+                  data-d8s-url-params="w_1002,h_600,c_pad"
+                ></div>
+                <div
+                  id="three-d-viewer"
+                  className="d8s-container"
+                  data-d8s-type="3d"
+                ></div>
+              </div>
+            </div>
+            <div className="product-details">
+              <p id="title-desktop" className="p-title">
+                Product Name Goes Here
+              </p>
+              <p className="p-price">$250.000</p>
+              <p className="p-description">
+                Introducing our Serenity Walk sneakers, designed for those who
+                value both style and comfort.
+              </p>
+              <p className="p-size">Size</p>
+              <div className="size-chart">
+                <div className="size-cell">36</div>
+                <div className="size-cell">36 2/3</div>
+                <div className="size-cell">37 1/3</div>
+                <div className="size-cell">38</div>
+                <div className="size-cell">38 2/3</div>
+                <div className="size-cell">39 1/3</div>
+                <div className="size-cell">40</div>
+                <button className="add-button">Add to Bag</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ public/product.js
@@ -0,0 +1,137 @@
+function runDimensions(id) {
+  if (!id) {
+    return;
+  }
+
+  const d8sApi = window.initDimensions({
+    account: "BlackrockDesign",
+    // account: "cloudinary-dimensions",
+    viewers: [
+      window.initDimensions.VIEWERS.IMAGE,
+      //   // window.initDimensions.VIEWERS.VIDEO,
+      window.initDimensions.VIEWERS.THREE_D,
+    ],
+    imageViewer: {
+      params: {},
+      className: "my-ecomm-image",
+    },
+    // videoViewer: {
+    // 	params: {
+    // 		autoplay: true,
+    // 		volume: 0,
+    // 		loop: true,
+    // 	},
+    // 	className: "my-ecomm-video",
+    // },
+    threeDViewer: {
+      viewer: {
+        controls: {
+          position: "center",
+        },
+        showLoadingProgress: false,
+      },
+    },
+    // baseUrl: "https://res.cloudinary.com/",
+  });
+
+  const unregister = d8sApi?.on((event, ...args) => {
+    const data = args[0];
+    if (data?.type === "MODEL_LOADED") {
+      document.getElementById("three-d-container")?.classList.add("show");
+    }
+  });
+  //unregister(); call to remove listener
+}
+
+function setProductTitle(id) {
+  const desktopTitle = document.getElementById("title-desktop");
+  const mobileTitle = document.getElementById("title-mobile");
+  desktopTitle.innerHTML = PRODUCTS_INFO[id].title;
+  mobileTitle.innerHTML = PRODUCTS_INFO[id].title;
+}
+
+function preloadProductZoomImage(id) {
+  ZOOM_IMAGE_URL = `https://dimensions-art.cloudinary.net/d8s-demo-site/image/upload/f_auto,q_auto,w_1000/${id}/shoes/`;
+  TEMPLATES.forEach((templateName) => {
+    const image = new Image();
+    image.src = ZOOM_IMAGE_URL + templateName;
+  });
+}
+
+function zoom(e) {
+  e.preventDefault();
+  const container = e.currentTarget;
+  let offsetX, offsetY;
+  e.offsetX ? (offsetX = e.offsetX) : (offsetX = e.touches?.[0].pageX);
+  e.offsetY ? (offsetY = e.offsetY) : (offsetY = e.touches?.[0].pageY);
+  const x = Math.min(Math.max((offsetX / container.offsetWidth) * 100, 0), 100);
+  const y = Math.min(
+    Math.max((offsetY / container.offsetHeight) * 100, 0),
+    100
+  );
+  container.style.backgroundPosition = x + "% " + y + "%";
+}
+
+function setZoomImage(e) {
+  const container = e.currentTarget;
+  const templateName = container.dataset.d8sName;
+  container.style.backgroundImage = `url(${ZOOM_IMAGE_URL}${templateName})`;
+}
+
+function removeZoomImage(e) {
+  const zoomer = e.currentTarget;
+  zoomer.style.backgroundImage = "none";
+}
+
+function initContainers(id) {
+  document.getElementById("three-d-viewer")?.setAttribute("data-d8s-id", id);
+
+  document.querySelectorAll("div.product-image")?.forEach((img) => {
+    img.setAttribute("data-d8s-id", id);
+
+    img.addEventListener("mouseenter", setZoomImage);
+    img.addEventListener("touchstart", setZoomImage);
+
+    img.addEventListener("mousemove", zoom);
+    img.addEventListener("touchmove", zoom);
+
+    img.addEventListener("mouseleave", removeZoomImage);
+    img.addEventListener("touchend", removeZoomImage);
+  });
+}
+
+let ZOOM_IMAGE_URL;
+const PRODUCTS_INFO = {
+  "DOG-ACC-SILVER-01": {
+    title: "Colorful Shoes",
+  },
+};
+const TEMPLATES = [
+  "sample-side-180",
+  "sample-top-305",
+  "sample-top-45",
+  "sample-bottom",
+  "sample-side-90",
+  "sample-stand-front",
+];
+
+// const sku = window.location.search
+//   .replace("?", "")
+//   .split("&")
+//   .map((pair) => pair.split("="))
+//   .filter(([key, value]) => key === "sku")?.[0]?.[1];
+
+// if (!sku) {
+//   console.warn("No product SKU present in the URL!!!");
+// } else {
+//   preloadProductZoomImage(sku);
+//   setProductTitle(sku);
+//   initContainers(sku);
+//   runDimensions(sku);
+// }
+
+//  preloadProductZoomImage("DOG-ACC-SILVER-01");
+//  setProductTitle("DOG-ACC-SILVER-01");
+
+initContainers("DOG-ACC-SILVER-01");
+runDimensions("DOG-ACC-SILVER-01");
